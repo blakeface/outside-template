3aa4c7b02677f0ebe04edbc644c5e9493119753f
netzebandito <blake.netzeband@gmail.com>
2018-07-19 15:17:42 -0600

    added styles to nav bar with cxs

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import data from '../data/navbar.js'
-import styles from '../stylesheets/navbar.css';
+import cxs from 'cxs/component'
+import WebFont from 'webfontloader';
+
+let font = String(data.styles.fontName.replace(/\s/g, '+'))
+if (data.styles.fontWeight.length) {
+	font += `:${data.styles.fontWeight}`
+}
+let fontColor = String(data.styles.fontColor)
+if (fontColor.split(',').length === 3) fontColor = `rgb(${fontColor})`
+if (fontColor.split(',').length === 4) fontColor = `rgba(${fontColor})`
+
+WebFont.load({
+  google: {
+    families: [font]
+  }
+});
+
+const StyledNav = cxs('nav')({
+	backgroundColor: data.styles.backgroundColor || 'black',
+	color: fontColor,
+	fontFamily: data.styles.fontName,
+	' img': {
+		width: '150px',
+	},
+	display: 'flex',
+	flexFlow: 'row noWrap',
+	justifyContent: 'space-around',
+	alignItems: 'center',
+})
 
 export default class Navbar extends Component {
-
 	renderNavItems(side) {
 		return data.sections.map((section, i, sections) => {
 			// return early for left or right side
-			if ( (side == 'left' && i + 1 <= Math.ceil(sections.length/2))
-				|| (side == 'right' && i + 1> Math.ceil(sections.length/2))
+			if ( (side == 'left' && i >= Math.ceil(sections.length/2))
+				|| (side == 'right' && i < Math.ceil(sections.length/2))
 			) {
 				return;
 			}
@@ -20,11 +47,12 @@ export default class Navbar extends Component {
 
 	render() {
 		return (
-			<nav className={styles.nav}>
+			<StyledNav>
 				{this.renderNavItems('left')}
 				<img src={"assets/images/" + data.image.fileName} alt={data.image.description} />
 				{this.renderNavItems('right')}
-			</nav>
+				{data.sections.length % 2 == 0 ? null : <div className="spacer" />}
+			</StyledNav>
 		)
 	}
 }
--- src/data/navbar.js
+++ src/data/navbar.js
@@ -1,23 +1,56 @@
-/** Heres the shape
+/** Here's the shape
 {
 	sections: [Array]
 	image: {
 		fileName: 'string',
 		description: 'string'
+	},
+	styles: {
+		background: {
+			color: 'string' (see note on color)
+		},
+		font: {
+			color: 'string' (see note on color)
+			name: 'string'(see not on name)
+			fallback: 'string' (not required)
+			italic: boolean (true or false)
+			weight: 'string'
+		}
 	}
 }
+
+* FONTS ARE FROM GOOGLE FONTS: https://fonts.google.com
+
+* COLORS CAN BE:
+hex ex: '#4286f4'
+rgb ex: '66, 134, 244'
+rgba ex: '66, 134, 244, 0.5'
+word ex: 'red', 'green', 'blue', ...
+
 */
 
 const data = {
+	// SECTION HEADERS
 	sections: [
 		'About',
 		'Services',
 		'Connect'
 	],
+
+	// IMAGE DETAILS
 	image: {
 		fileName: 'navimage.png',
 		description: 'company logo',
+	},
+
+	// SECTION STYLES
+	styles: {
+		backgroundColor: 'black',
+		fontColor: 'white',
+		fontName: 'Roboto', // Google Fonts
+		fontWeight: '' // 100 - 900
 	}
+
 }
 
 export default data
